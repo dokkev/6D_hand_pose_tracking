ceffafea0f119aec5acd0c8b41413653dda7e627
dokkev <dhk6869@gmail.com>
2023-02-19 20:23:56 -0600

    demo_ready

---
+++ webcam/3d_tracking.py
@@ -0,0 +1,111 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+  
+# print("Launchmode:", sys.argv[1])
+
+class HandTracking():
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(static_image_mode=False,
+                                              max_num_hands=1,           
+                                              min_detection_confidence=0.5,   
+                                              min_tracking_confidence=0.5) 
+        self.mp_draw = mp.solutions.drawing_utils
+        self.mp_styles = mp.solutions.drawing_styles
+
+    def findHands(self, img):
+      
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        # flip the image horizontally for a selfie-view display.
+        img = cv2.flip(img, 1)
+        self.results = self.hands.process(img)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+
+
+        if self.results.multi_hand_landmarks:
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                self.mp_draw.draw_landmarks(
+                    img, 
+                    hand_landmarks, 
+                    self.mp_hands.HAND_CONNECTIONS,
+                    self.mp_styles.get_default_hand_landmarks_style(), 
+                    self.mp_styles.get_default_hand_connections_style()
+                )        
+        return img
+
+    def findPosition(self):
+        data = []
+        # print(self.results.multi_handedness)
+        if self.results.multi_hand_landmarks:
+            # print(self.results.multi_hand_landmarks)
+            for hand_landmarks in self.results.multi_hand_landmarks:
+                for id, lm in enumerate(hand_landmarks.landmark):
+                    data.append([lm.x, lm.y, lm.z])
+            # for hand_world_landmarks in self.results.multi_hand_world_landmarks:
+            #     for id, lm in enumerate(hand_world_landmarks.landmark):
+            #         data.append([lm.x, lm.y, lm.z])
+
+        data = np.array(data)
+        return data
+            
+
+    
+    def displayFPS(self, cap, img):
+        cv2.putText(img, f'FPS: {int(cap.get(cv2.CAP_PROP_FPS))}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        return img
+    
+
+    def plot(self,ax,plt,data):
+        # Create 3D plot
+
+        if self.results.multi_hand_landmarks:
+            # Clear the plot and add new data
+            ax.clear()
+            ax.set_xlim3d(-0.5, 0.5)
+            ax.set_ylim3d(-0.5, 0.5)
+            ax.set_zlim3d(-0.5, 0.5)
+            ax.scatter3D(*zip(*data))
+
+     
+            edges = [(1,2),(2,3),(3,4),(0,5),(5,6),(5,9),(1,0),(6,7),(7,8),(0,9),(9,10),(10,11),(11,12),(9,13),(13,14),(14,15),(15,16),(13,17),(17,18),(18,19),(19,20),(0,17)]
+
+            for edge in edges:
+                ax.plot3D(*zip(data[edge[0]], data[edge[1]]), color='red')
+      
+            # Draw the plot
+            plt.draw()
+            plt.pause(0.0001)
+    
+def main():
+    cap = cv2.VideoCapture(0)
+    detector = HandTracking()
+
+    # Create 3D plot
+    fig = plt.figure()
+    plt.ion()
+    ax = fig.add_subplot(111, projection='3d')
+
+    while True:
+        success, img = cap.read()
+        img = detector.findHands(img)
+        
+        data = detector.findPosition()
+        detector.plot(ax,plt,data)
+
+
+
+        img = detector.displayFPS(cap,img)
+        cv2.imshow("MediaPipe Hands", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
+    
+
+
--- webcam/webcam.py
+++ webcam/webcam.py
@@ -1,64 +1,67 @@
 import cv2
 import mediapipe as mp
-import sys
 
-# Load the MediaPipe hand tracking model
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# Initialize the hand tracking module
-hands = mp_hands.Hands(
-    static_image_mode=False,
-    max_num_hands=1,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
-)
 
-# Open the video stream
+# Initialize the parameters for the hands tracking
+hands = mp_hands.Hands(
+    static_image_mode=False,        # If set to false, the solution treats the input images as a video stream. 
+                                    # It will try to detect hands in the first input images, and upon a successful detection further localizes the hand landmarks. 
+                                    # In subsequent images, once all max_num_hands hands are detected and the corresponding hand landmarks are localized, 
+                                    # it simply tracks those landmarks without invoking another detection until it loses track of any of the hands. 
+                                    # This reduces latency and is ideal for processing video frames. If set to true, hand detection runs on every input image, 
+                                    # ideal for processing a batch of static, possibly unrelated, images.
+
+    max_num_hands=1,                # Maximum number of hands to detect
+
+    min_detection_confidence=0.5,   # Minimum confidence value ([0.0, 1.0]) from the hand detection model for the detection to be considered successful.
+
+    min_tracking_confidence=0.5     # Minimum confidence value ([0.0, 1.0]) from the landmark-tracking model for the hand landmarks to be considered tracked successfully, 
+                                    # or otherwise hand detection will be invoked automatically on the next input image. 
+                                    # Setting it to a higher value can increase robustness of the solution, at the expense of a higher latency. 
+    )                               # Ignored if static_image_mode is true, where hand detection simply runs on every image. 
+
+#  Define the camera with cv2
 cap = cv2.VideoCapture(0)
 
-while True:
-    # Read a frame from the video stream
+
+while cap.isOpened():
     success, image = cap.read()
+    # Exit the loop if no image is captured
     if not success:
+        print("Error: No Camera Found")
         break
 
-    # Convert the image to RGB format
+    # Convert the image to RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-    # Run the hand tracking model on the image
+    # Process the image
     results = hands.process(image)
-
-    # Extract the 6D hand pose from the results
-    if results.multi_hand_landmarks:
-        hand_landmarks = results.multi_hand_landmarks[0]
-        pose = []
-        for lm in hand_landmarks.landmark:
-            x = lm.x
-            y = lm.y
-            z = lm.z
-
-        # print x, y, z without printing a new line
-        sys.stdout.write("\r%f %f %f" % (x, y, z))
-        sys.stdout.flush()
-        
-
-
-       
-
-
-       
-
-    # Display the image with the hand pose
+    # convert the image back to BGR
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    # Collection of detected/tracked hands, where each hand is represented as a list of 21 hand landmarks
     if results.multi_hand_landmarks:
-        mp.solutions.drawing_utils.draw_landmarks(
-            image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-    cv2.imshow("Hand Pose Tracking", image)
+        # for-loop for each landmark of each hand
+        for hand_landmarks in results.multi_hand_landmarks:
+            # Draw the landmarks
+            mp_drawing.draw_landmarks(
+                image, # define the image
+                hand_landmarks, # display dots on the landmarks
+                mp_hands.HAND_CONNECTIONS, # connect the dots between the landmarks
+                mp_drawing_styles.get_default_hand_landmarks_style(), # change the color of dots for different regions of the hand
+                mp_drawing_styles.get_default_hand_connections_style() # change the color of the lines between the dots
+            )
+            
+    
+    # Flip the image horizontally for a selfie-view display.
+    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    # Display FPS
+    cv2.putText(image, f'FPS: {int(cap.get(cv2.CAP_PROP_FPS))}', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
-    # Exit on key press
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # Exit the loop if the user press the 'Esc' key
+    if cv2.waitKey(5) & 0xFF == 27:
         break
-
-# Release the video stream and the hand tracking module
 cap.release()
-hands.close()
